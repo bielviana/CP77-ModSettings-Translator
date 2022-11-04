b2053a05618599f7c75d8fee923c847d9af51beb
Gabriel Viana <53864824+pySiriusDev@users.noreply.github.com>
2022-11-04 03:40:45 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,32 +4,33 @@ from os import path as p, system as cmd
 class Translator():
     def __init__(self) -> None:
         self.modFilePath = input('Path to .reds file: ')
-        self.jsonFilePath = input('path to .json file: ')
-        self.modFile = ''
-        self.jsonFile = ''
+        self.langCode = input('Language code (ex: en-us): ')
 
         self.modFileName = self.modFilePath.split('\\')
         self.modFileName = self.modFileName[::-1][0]
         
         self.modFileDir = self.modFilePath.replace(self.modFileName, '')
 
+        self.jsonFileName = f'{self.langCode}.json'
+        self.jsonFilePath = p.join(self.modFileDir, 'localization', self.jsonFileName)
+
     def ReadMod(self):
-        with open(self.modFilePath, 'r') as file:
+        with open(self.modFilePath, 'r', encoding='utf-8') as file:
             self.modFile = file.read()
     
     def ReadJson(self):
-        with open(self.jsonFilePath, 'r') as file:
+        with open(self.jsonFilePath, 'r', encoding='utf-8') as file:
             self.jsonFile = json.load(file)
 
     def TranslateMod(self):
-        for i in self.jsonFile['entries']:
-            self.modFile = self.modFile.replace(self.jsonFile['entries'][i]['key'], self.jsonFile['entries'][i]['value'])
+        for entry in self.jsonFile['entries']:
+            self.modFile = self.modFile.replace(entry['key'], entry['value'])
 
     def RewriteMod(self):
         newModFileDir = p.join(self.modFileDir, 'Localization', self.jsonFile['lang'])
         newModFilePath = p.join(newModFileDir, self.modFileName)
         cmd(f'md {newModFileDir}')
-        with open(newModFilePath, 'w') as file:
+        with open(newModFilePath, 'w', encoding='utf-8') as file:
             file.write(self.modFile)
 
 
